8f2e101d08a9895ede9ebc1a3d6b89152549bdfc
21A91A1255 <97424886+21A91A1255@users.noreply.github.com>
2024-02-18 21:18:14 +0530

    Time: 4 ms (96.03%) | Memory: 16.2 MB (12.95%) - LeetSync

---
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& arr) {
+        int m=arr.size(),n=arr[0].size();
+        vector<int> row(m, 0);
+        vector<int> col(n, 0);
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(arr[i][j]==0)
+                {
+                    row[i]=1;
+                    col[j]=1;
+                }
+            }
+        }
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(row[i]==1 or col[j]==1)
+                {
+                    arr[i][j]=0;
+                }
+            }
+        }
+        return ;
+    }
+};
