efd371f7df27d24440e8435d02b0f61852d6647d
21A91A1255 <97424886+21A91A1255@users.noreply.github.com>
2024-02-22 11:34:42 +0530

    Time: 608 ms (53.32%) | Memory: 21.7 MB (66.79%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.py
+++ 1039-find-the-town-judge/find-the-town-judge.py
@@ -1,22 +1,15 @@
 from collections import Counter
 class Solution:
     def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        l=[]
-        m=[]
-        if(n==1):
-            return 1
-        if(n==4 and trust==[[1,3],[1,4],[2,3]]):
-            return -1
-        if(n==11 and trust==[[1,8],[1,3],[2,8],[2,3],[4,8],[4,3],[5,8],[5,3],[6,8],[6,3],[7,8],[7,3],[9,8],[9,3],[11,8],[11,3]]):
-            return -1
-        for i in range(len(trust)):
-            k=trust[i]
-            l.append(k[0])
-            m.append(k[1])
-        c=Counter(m)
-        s=set(l)
-        for i in c.keys():
-            print(i,c[i])
-            if(c[i]==len(s)):
+        l=[0]*(n+1)
+        m=[0]*(n+1)
+        for i in trust:
+            p=i[0]
+            q=i[1]
+            l[p]+=1
+            m[q]+=1
+        for i in range(1,(n+1)):
+            if(l[i]==0 and m[i]==n-1):
                 return i
         return -1
+        
