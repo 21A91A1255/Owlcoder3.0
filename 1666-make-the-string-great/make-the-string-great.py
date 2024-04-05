a28d8c87197f44efed7f9aaaff64ffd79a6a37af
21A91A1255 <97424886+21A91A1255@users.noreply.github.com>
2024-04-05 16:02:12 +0530

    Time: 36 ms (69.21%) | Memory: 16.5 MB (46.83%) - LeetSync

---
+++ 1666-make-the-string-great/make-the-string-great.py
@@ -0,0 +1,11 @@
+class Solution:
+    def makeGood(self, s: str) -> str:
+        st=[]
+        for i in range(len(s)):
+            if st and abs(ord(s[i])-ord(st[-1]))==32:
+                st.pop()
+            else:
+                st.append(s[i])
+        p=''.join(st)
+        return p        
+        
