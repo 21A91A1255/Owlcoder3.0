66caf6b3b41196ef9f6b50e96100cc0cc155dbf9
21A91A1255 <97424886+21A91A1255@users.noreply.github.com>
2025-07-07 21:34:35 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Next Greater Element in Circular Array/next-greater-element-in-circular-array.py
@@ -0,0 +1,20 @@
+class Solution:
+    def nextLargerElement(self, arr):
+        ans = []
+        st = []
+        r=arr+arr
+        for i in range(len(r) - 1, -1, -1):
+            while st and st[-1] <= r[i]:
+                st.pop()
+
+            if not st:
+                ans.append(-1)
+            else:
+                ans.append(st[-1])
+
+            st.append(r[i])
+        ans=ans[::-1]
+        ans=ans[:len(arr)]
+        return ans
+
+                
